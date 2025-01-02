ab145edcd11d2fb1d02b24e3f59bc407738618dc
Ehehing <ruiruithing@gmail.com>
2025-01-01 18:18:18 -0800

    rizz1

--- Board.java
+++ Board.java
@@ -18,58 +18,92 @@
 
 public class Board {
 
+    private Node[] board;
+    private int bLength;
 
-    private int[][] boardTiles;
-    private int n;
-    private int[][] finishedTiles;
-    
+    private static class Node {
+        private int row;
+        private int col;
+        private int value;
 
+    }
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
 
+        bLength = tiles.length;
+        board = new Node[bLength*2];
+        int cur = 0;
+        for (int i = 0; i<bLength; i++) {
+            for (int j = 0; j<bLength; j++) {
 
-        n = tiles.length;
+                board[cur].value = tiles[i][j];
+                board[cur].row = i+1;
+                board[cur].col = j+1;
 
-        boardTiles = tiles;
-        finishedTiles = new int[n][n];
-        int k = 1;
-        for (int i = 0; i<n; i++) {
-            for (int j = 0; j<n; j++) {
-                finishedTiles[i][j] = k;
+                cur++;
             }
         }
-        finishedTiles[n-1][n-1] = 0;
 
     }
                                            
     // string representation of this board
-    public String toString() {
-
+    public String toString() { //convert to legible form
+        StringBuilder s = new StringBuilder();
+    s.append(n + "\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            s.append(String.format("%2d ", tiles[i][j]));
+        }
+        s.append("\n");
+    }
+    return s.toString();
     }
 
     // board dimension n
     public int dimension() {
-
+        return bLength*2;
     }
 
     // number of tiles out of place
     public int hamming() {
-
+        int right = 1;
+        int ham = 0;
+        for (int i = 0; i<dimension()-1; i++) {
+            if (board[i].value != right) {
+                ham++;
+            }
+        }
+        if (board[dimension()-1].value == 0) {
+            return ham;
+        }
+            return ham+1;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
 
-        for (int i = 0; i<boardTiles.length; i++) {
-
-            if (boardTiles[])
-
-
-        }
+        
 
     }
+
+    /**
+     *    1 2 3
+     *    4 5 6 
+     *    7 8 0
+     * 
+     * @param row
+     * @param col
+     * @return
+     */
+
+
+    private int getIndex(int row, int col) {
+        return (row-1) * bLength + col; 
+    } 
+
+
 
     // is this board the goal board?
     public boolean isGoal() {
